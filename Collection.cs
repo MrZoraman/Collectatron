b70862e38f1a1cd0d95e547e70192d2873e41d67
MrZoraman <MrZoraman@gmail.com>
2023-10-18 19:38:46 -0700

    Start on data model classes.

---
+++ Collection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Collectatron
+{
+    internal class Collection
+    {
+        private List<CollectionItem> _items = new();
+
+        public void LoadItems()
+        {
+            var item1 = AddItem();
+            item1.Name = "Item 1";
+        }
+
+        public CollectionItem AddItem()
+        {
+            var item = new CollectionItem(this, GetNextId());
+            _items.Add(item);
+            return item;
+        }
+
+        private int GetNextId()
+        {
+            if (_items.Count == 0)
+            {
+                return 0;
+            }
+
+            var highestId = _items.Max(ci => ci.Id);
+            return highestId + 1;
+        }
+    }
+}
---
+++ CollectionItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Collectatron
+{
+    internal class CollectionItem
+    {
+        public CollectionItem(Collection collection, int id)
+        {
+            _collection = collection;
+            Id = id;
+        }
+
+        private readonly Collection _collection;
+
+        public int Id { get;} = 0;
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal? PricePaid { get; set; }
+
+        public decimal? EstimatedValue { get; set; }
+
+        public int? Year { get; set; }
+
+        public string Comments { get; set; } = string.Empty;
+    }
+}
